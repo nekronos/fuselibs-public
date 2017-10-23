fuse-open/fuselibs

Fix Navigator.Pages subscription

Source/Fuse.Controls.Navigation/NavigationControl.Pages.uno
@@ -40,7 +40,7 @@ namespace Fuse.Controls
 			if (_pages == null)
 				return;
 				
-			var obs = _pages as IObservable;
+			var obs = _pages as IObservableArray;
 			if (obs != null)
 				_pagesSubscription = obs.Subscribe(this);
 				

nekronos/fuselibs-public

Source/Fuse.Controls.Navigation/NavigationControl.Pages.uno
@@ -34,8 +34,8 @@ namespace Fuse.Controls
 			
 			if (_pageHistory == null)
 				return;
-			
-			var obs = _pageHistory as IObservable;
+				
+			var obs = _pageHistory as IObservableArray;
 			if (obs != null)
 			{
 				AncestorRouterPage.ChildRouterPages.Attach( obs, this );